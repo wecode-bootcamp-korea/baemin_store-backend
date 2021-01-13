2b8daf8eda4836a68f189cf6f8e32547554fd41e
이혜성 <hyeseong43@gmail.com>
2021-01-14 01:18:45 +0900

    리뷰 사항 수정 완료

--- product/models.py
+++ product/models.py
@@ -1,46 +1,48 @@
 from django.db import models
 
 class Category(models.Model):
-	name	= models.CharField(max_length	=45)
+	name = models.CharField(max_length	=45)
 
 	class Meta: 
 		db_table = 'categories'
 
 class Product(models.Model):
-	user          = models.ForeignKey('user.User', on_delete=models.CASCADE)
-	category		  = models.ForeignKey('Category', on_delete=models.CASCADE)
+	category	  = models.ForeignKey('Category', on_delete=models.CASCADE)
 	name          = models.CharField(max_length=45)
-	main_image    = models.DecimalField(max_digits=18, decimal_places=2)
+	main_image	  = models.URLField(max_length=2000)
+	price		  = models.DecimalField(max_digits=18, decimal_places=2, default=0)
 	money_replace = models.CharField(max_length=45)
-	create_at     = models.DateTimeField(auto_now_add=True)
+	create_at 	  = models.DateTimeField(auto_now_add=True)
 	update_at     = models.DateTimeField(auto_now=True)
+	tags 		  = models.ManyToManyField('Tag', through='ProductTag')
+	hashtags 	  = models.ManyToManyField('HashTag', through='ProductHashTag')
+	sales 		  = models.ManyToManyField('Sale', through='ProductSale')
 
 	class Meta: 
 		db_table = 'products'
 
 class Stock(models.Model):
-	product		= models.ForeignKey('Product', on_delete=models.CASCADE)
-	quantity	= models.IntegerField(default=0)
+	product	 = models.ForeignKey('Product', on_delete=models.CASCADE)
+	quantity = models.IntegerField(default=0)
 
 	class Meta:
 		db_table = 'stocks'
 	
 class ProductTag(models.Model):
-	product	= models.ForeignKey('user.User', on_delete=models.CASCADE)
-	tag_id  = models.ForeignKey('Tag', on_delete=models.CASCADE)
+	product	= models.ForeignKey('Product', on_delete=models.CASCADE)
+	tag		= models.ForeignKey('Tag', on_delete=models.CASCADE)
 	
 	class Meta: 
 		db_table = 'product_tags'
 
 class Tag(models.Model):
-	tag_name = models.CharField(max_length=45)
+	name = models.CharField(max_length=45)
 	
 	class Meta: 
 		db_table = 'tags'		
 
 
-class ProductsHashTag(models.Model):
-	name 		= models.CharField(max_length=45)
+class ProductHashTag(models.Model):
 	product	= models.ForeignKey('Product', on_delete=models.CASCADE)
 	hashtag	= models.ForeignKey('HashTag', on_delete=models.CASCADE)
 
@@ -57,7 +59,7 @@ class HashTag(models.Model):
 
 class ProductSale(models.Model):
 	product	= models.ForeignKey('Product', on_delete=models.CASCADE)
-	sale 		= models.ForeignKey('Sale', on_delete=models.CASCADE)
+	sale 	= models.ForeignKey('Sale', on_delete=models.CASCADE)
 
 	class Meta: 
 		db_table = 'product_sales'
@@ -71,7 +73,7 @@ class Sale(models.Model):
 
 
 class DetailImage(models.Model):
-	product	= models.ForeignKey('Product', on_delete=models.CASCADE)
+	product		= models.ForeignKey('Product', on_delete=models.CASCADE)
 	detail_url	= models.CharField(max_length=45)
 	image_flow	= models.IntegerField(default=0)
 
@@ -80,29 +82,30 @@ class DetailImage(models.Model):
 
 
 class Like(models.Model):
-  user			= models.ForeignKey('user.User',  on_delete = models.CASCADE)
-  product 	= models.ForeignKey('Product',  on_delete = models.CASCADE)
+	user	 = models.ForeignKey('user.User',  on_delete=models.CASCADE)
+	product  = models.ForeignKey('Product',  on_delete=models.CASCADE)
+	liked    = models.BooleanField(default= True)
 
-  class Meta:
-    db_table  = 'likes'
+	class Meta:
+		db_table  = 'likes'
 
 class Review(models.Model):
-  user    		= models.ForeignKey('user.User',  on_delete = models.CASCADE)
-  product  		= models.ForeignKey('Product',   on_delete = models.CASCADE)
-  description = models.TextField()
-  create_at   = models.DateField(auto_now_add = True)
-  update_at   = models.DateField(auto_now     = True)
-
-  class Meta:
-    db_table  = 'reviews'
+	user		= models.ForeignKey('user.User',  on_delete=models.CASCADE)
+	product		= models.ForeignKey('Product',   on_delete=models.CASCADE)
+	description	= models.TextField()
+	create_at	= models.DateField(auto_now_add=True)
+	update_at	= models.DateField(auto_now=True)
+	
+	class Meta:
+		db_table  = 'reviews'
 
 
 class ReviewImage(models.Model):
-  review	= models.ForeignKey('Review',     on_delete = models.CASCADE)
-  image_url	= models.URLField(max_length=255)
+	review	  = models.ForeignKey('Review', on_delete=models.CASCADE)
+	image_url = models.URLField(max_length=2048)
 
-  class Meta:
-    db_table  = 'review_images'
+	class Meta:
+		db_table  = 'review_images'
 
 	
 	
--- user/models.py
+++ user/models.py
@@ -1,33 +1,34 @@
 from django.db import models
 
 class User(models.Model):
-  account   = models.CharField(max_length   = 45)
-  password  = models.CharField(max_length   = 255)
-  name      = models.CharField(max_length   = 50)
-  email     = models.EmailField(max_length  = 45)
-  phone     = models.CharField(max_length   = 11)
-  create_at = models.DateField(auto_now_add = True)
-  update_at = models.DateField(auto_now     = True)
+    account      = models.CharField(max_length=45)
+    password     = models.CharField(max_length=255)
+    name         = models.CharField(max_length=50)
+    email        = models.EmailField(max_length=45)
+    phone        = models.CharField(max_length=11)
+    create_at    = models.DateField(auto_now_add=True)
+    update_at    = models.DateField(auto_now=True)
+    liked        = models.ManyToManyField('product.Product', through='product.Like')
 
-  class Meta:
-    db_table = 'users'
+    class Meta:
+      db_table = 'users'
 
 
 class Address(models.Model):
-  user      = models.ForeignKey('User', on_delete = models.CASCADE)
-  address   = models.CharField(max_length   = 100)
-  reciever  = models.CharField(max_length   = 45)
+    user     = models.ForeignKey('User', on_delete=models.CASCADE)
+    name     = models.CharField(max_length=100)
+    reciever = models.CharField(max_length=45)
 
-  class Meta:
-    db_table = 'addresses'
+    class Meta:
+      db_table = 'addresses'
 
 class Coupon(models.Model):
-  user        = models.ForeignKey('User', on_delete = models.CASCADE)
-  min_ammount = models.DecimalField(max_digits = 18, decimal_places = 2)
-  coupon_rate = models.DecimalField(max_digits =  3, decimal_places = 2)
-  name        = models.CharField(max_length    = 45)
-  create_at   = models.DateField(auto_now_add  = True)
-  update_at   = models.DateField(auto_now_add  = True)
+    user        = models.ForeignKey('User', on_delete=models.CASCADE)
+    min_ammount = models.DecimalField(max_digits=18, decimal_places=2)
+    coupon_rate = models.DecimalField(max_digits=3, decimal_places=2)
+    name        = models.CharField(max_length=45)
+    create_at   = models.DateField(auto_now_add=True)
+    update_at   = models.DateField(auto_now_add=True)
 
-  class Meta:
-    db_table= 'coupons'
+    class Meta:
+      db_table= 'coupons'
