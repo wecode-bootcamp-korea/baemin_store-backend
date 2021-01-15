a6896736f33ac4b1598e328f66a82a47e8c16749
이혜성 <hyeseong43@gmail.com>
2021-01-15 23:27:06 +0900

    [UPDATE][PR REVIEW] SINGN-UP&LOGIN VIEW 1. VALIDATION을 위한 함수들을 별도로 모듈화함 2. 불필요한 코드 리팩토링(예. User.objects.get() 3. 잘못 작성된 login  decorator 수정 4. `if utils.password_check() 문을 사용하지 않고 함수 호출 바로 시행 5. 어떠한 토큰을 사용하였는지 구체화하기 위해 변수명 재설정(login_token)

--- user/views.py
+++ user/views.py
@@ -7,7 +7,8 @@ from django.http    import JsonResponse
 from django.views   import View
 
 from user.models    import User
-from decorators     import utils
+from user           import utils
+
 from my_settings    import SECRET_KEY, ALGORITHM
 
 
@@ -24,8 +25,7 @@ class SignUpView(View):
             if not utils.account_validation(account): 
                 return JsonResponse({"MESSAGE":'NOT VALID ACCOUNT'}, status=400)
 
-            if utils.password_check(password)!=True:
-                return utils.password_check(password)
+            utils.password_check(password)
 
             if not utils.email_validation(email):
                 return JsonResponse({"MESSAGE":'NOT VALID EMAIL'}, status=400)
@@ -59,22 +59,24 @@ class SignUpView(View):
 class LoginView(View):
     def post(self, request):
         try:
-            data        = json.loads(request.body)
-            signup_db   = User.objects.all()
-            password    = data['password']
-            
-            if not signup_db.filter(account=data['account']).exists():
-                return JsonResponse({'MESSAGE':'ACCOUNT NOT FOUND'}, status=404)
+            data      = json.loads(request.body)
+            account   = data['account']
+            password  = data['password']
 
-            if signup_db.filter(account = data['account']).exists():
-                login_id = User.objects.get(account = data['account'])
+            user           = User.objects.get(account=account)
+            password_in_db = user.password
 
-                if bcrypt.checkpw(password.encode('utf-8'), login_id.password.encode('utf-8')):
-                    user_token = jwt.encode({'user_id': login_id.id}, SECRET_KEY, algorithm=ALGORITHM)
-                    return JsonResponse({'TOKEN':user_token}, status=200)
-                return JsonResponse({'MESSAGE':'INVALID ACCOUNT'}, status= 401)
+
+            if bcrypt.checkpw(password.encode('utf-8'), password_in_db.encode('utf-8')):
+                payload = {"user_id":user.id}
+                login_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+                return JsonResponse({'MESSAGE':'SUCCESS','TOKEN':login_token}, status=200, safe=True)
+            return JsonResponse({'MESSAGE':'INVALID ACCOUNT'}, status= 401)
 
         except KeyError :
             return JsonResponse({'MESSAGE':'KEY ERROR'}, status=400)
         except ValueError:
-            return JsonResponse({'MESSAGE':'VALUE ERROR'}, status=400)
+            return JsonResponse({'MESSAGE':'VALUE ERROR'},status=400)
+        except User.DoesNotExist:
+            return JsonResponse({"error": "ACCOUNT NOT EXISTS."}, status=404)
+
